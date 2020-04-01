36dbbb6baafa5a2575934a015198e05dbac4a068
Zaine Nascimento <amandazaine@outlook.com>
2020-04-01 16:50:28 -0300

    Terceiro commit: tirando comentário de teste

--- src/index.js
+++ src/index.js
@@ -7,29 +7,17 @@ function getText() {
     
     const textEorD = document.getElementById("insertTextForCD").value;         
     const offsetValue = document.getElementById("insertOffset").value;   
-    //alert(textEorD);
-    //alert(offsetValue);
-
+    
     if(document.activeElement.id === "encodeButton"){
 
         alert("Encode");
-
-        //let encodeA = cipher.encode(offsetValue, textEorD);
-        //alert(encodeA);
-
-        //alert(textEorD);
-        //alert(offsetValue);
+        
         document.getElementById("showTextCD").innerHTML = cipher.encode(offsetValue, textEorD);
 
     }else{
 
         alert("Decode");
-
-        //let decodeA = cipher.decode(offsetValue, textEorD);
-        //alert(decodeA);
-
-        //alert(textEorD);
-        //alert(offsetValue);
+       
         document.getElementById("showTextCD").innerHTML = cipher.decode(offsetValue, textEorD);
 
     }
