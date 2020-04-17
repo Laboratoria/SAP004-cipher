23f97c996066a23a3d3a0659312cc1f11cc003b3
Zaine Nascimento <amandazaine@outlook.com>
2020-04-17 00:11:23 -0300

    Refatorando códigos

--- src/cipherHacker.js
+++ src/cipherHacker.js
@@ -8,18 +8,89 @@ const cipher = {
             
         let stringOutput = [];
      
-        for(let i = 0; i < stringInput.length; i++) {
+        for(let encodeIn of stringInput) {
             
-            if(stringInput.charCodeAt(i) >= 65 && stringInput.charCodeAt(i) <= 90){                
-                let x = Math.abs(offset);
-                let y = (stringInput.charCodeAt(i));      
-                let z = ((((y - 65) + x) % 26) + 65);
+            if(encodeIn.charCodeAt() >= 65 && encodeIn.charCodeAt() <= 90){  
+                                  
+                const z = (((((encodeIn.charCodeAt()) - 65) + Math.abs(offset)) % 26) + 65);
                 stringOutput.push(String.fromCharCode(z));
               
-            }else if(stringInput.charCodeAt(i) >= 97 && stringInput.charCodeAt(i) <= 122){
-                let x = Math.abs(offset);
-                let y = (stringInput.charCodeAt(i));      
-                let z = ((((y - 97) + x) % 26) + 97);
+            }else if(encodeIn.charCodeAt() >= 97 && encodeIn.charCodeAt() <= 122){  
+
+                const z = (((((encodeIn.charCodeAt()) - 97) + Math.abs(offset)) % 26) + 97);
+                stringOutput.push(String.fromCharCode(z));
+            }else{
+                stringOutput.push(String.fromCharCode(encodeIn.charCodeAt()));  
+            }            
+        }   
+        let encodedText = stringOutput.join('');  
+        
+        return encodedText;           
+    },
+  
+    decode: function(offset, stringInput) {
+
+        if (offset == "" || typeof stringInput !== "string") {
+            throw new TypeError('Insira o número do offset');
+        }
+      
+        let stringOutput = [];       
+        
+        for(let decodeOut of stringInput) {
+    
+            if(decodeOut.charCodeAt() >= 65 && decodeOut.charCodeAt() <= 90){
+                
+                const z = (((((decodeOut.charCodeAt()) + 65) - Math.abs(offset)) % 26) + 65);
+                stringOutput.push(String.fromCharCode(z));
+            
+            }else if(decodeOut.charCodeAt() >= 97 && decodeOut.charCodeAt() <= 122){
+
+                const z = (((((decodeOut.charCodeAt()) - 122) - Math.abs(offset)) % 26) + 122);
+                stringOutput.push(String.fromCharCode(z)); 
+            }else{        
+                stringOutput.push(String.fromCharCode(decodeOut.charCodeAt()));  
+            }       
+        }
+        let decodedText = stringOutput.join('');
+        
+        return decodedText;
+      }
+   };
+    
+    export default cipher;
+
+
+
+
+
+
+
+
+
+
+
+    /*
+
+    const cipher = {
+    
+    encode: function(offset, stringInput) {
+
+        if (offset == "" || typeof stringInput !== "string") {
+            throw new TypeError('Insira o número do offset');
+        }
+            
+        let stringOutput = [];
+     
+        for(let i = 0; i < stringInput.length; i++) {
+            
+            if(stringInput.charCodeAt(i) >= 65 && stringInput.charCodeAt(i) <= 90){  
+                                  
+                const z = (((((stringInput.charCodeAt(i)) - 65) + Math.abs(offset)) % 26) + 65);
+                stringOutput.push(String.fromCharCode(z));
+              
+            }else if(stringInput.charCodeAt(i) >= 97 && stringInput.charCodeAt(i) <= 122){  
+
+                const z = (((((stringInput.charCodeAt(i)) - 97) + Math.abs(offset)) % 26) + 97);
                 stringOutput.push(String.fromCharCode(z));
             }else{
                 stringOutput.push(String.fromCharCode(stringInput.charCodeAt(i)));  
@@ -27,9 +98,7 @@ const cipher = {
         }
         let encodedText = stringOutput.join('');  
         
-        return encodedText; 
-    
-              
+        return encodedText;           
     },
   
     decode: function(offset, stringInput) {
@@ -43,15 +112,13 @@ const cipher = {
         for(let i = 0; i < stringInput.length; i++) {
     
             if(stringInput.charCodeAt(i) >= 65 && stringInput.charCodeAt(i) <= 90){
-                let x = Math.abs(offset);
-                let y = (stringInput.charCodeAt(i));      
-                let z = ((((y + 65) - x) % 26) + 65);
+                
+                const z = (((((stringInput.charCodeAt(i)) + 65) - Math.abs(offset)) % 26) + 65);
                 stringOutput.push(String.fromCharCode(z));
             
             }else if(stringInput.charCodeAt(i) >= 97 && stringInput.charCodeAt(i) <= 122){
-                let x = Math.abs(offset);
-                let y = (stringInput.charCodeAt(i));      
-                let z = ((((y - 122) - x) % 26) + 122);
+
+                const z = (((((stringInput.charCodeAt(i)) - 122) - Math.abs(offset)) % 26) + 122);
                 stringOutput.push(String.fromCharCode(z)); 
             }else{        
                 stringOutput.push(String.fromCharCode(stringInput.charCodeAt(i)));  
@@ -61,7 +128,8 @@ const cipher = {
         
         return decodedText;
       }
-  
    };
     
     export default cipher;
+
+    */
--- src/index.js
+++ src/index.js
@@ -1,57 +1,50 @@
 import cipher from './cipherHacker.js';
 
-document.getElementById("encodeButton").addEventListener("click",getText);
-document.getElementById("decodeButton").addEventListener("click",getText);
+document.getElementById("encode-button").addEventListener("click",getText);
+document.getElementById("decode-button").addEventListener("click",getText);
 
-function getText() {
+document.getElementById("encode-button").addEventListener("mouseover",mouseOverColorE);
+document.getElementById("decode-button").addEventListener("mouseover",mouseOverColorD);
+ 
+document.getElementById("encode-button").addEventListener("mouseout",mouseOutColorE);
+document.getElementById("decode-button").addEventListener("mouseout",mouseOutColorD);
+
+
+function getText() {    
+    const textEorD = document.getElementById("insert-text-for-cd").value;         
+    const offsetValue = Number(document.getElementById("insert-offset").value);   
     
-    const textEorD = document.getElementById("insertTextForCD").value;         
-    const offsetValue = Number(document.getElementById("insertOffset").value);   
-    
-    if(document.activeElement.id === "encodeButton"){
+    if(document.activeElement.id === "encode-button"){
                
-        document.getElementById("showTextCD").innerHTML = cipher.encode(offsetValue, textEorD);
+        document.getElementById("show-text-cd").innerHTML = cipher.encode(offsetValue, textEorD);
 
     }else{        
-       
-        document.getElementById("showTextCD").innerHTML = cipher.decode(offsetValue, textEorD);
-
+        document.getElementById("show-text-cd").innerHTML = cipher.decode(offsetValue, textEorD);
     }
+}
 
- }
+function mouseOverColorE() {
+    document.getElementById("encode-button").style.backgroundColor = '#fbf7fc';
+    document.getElementById("encode-button").style.color = '#626fa6';
+    document.getElementById("encode-button").style.border = '#626fa6';
+    document.getElementById("encode-button").style.border = '1px solid #626fa6'; 
+}
 
- // Adicionando interações nos botões
-
- document.getElementById("encodeButton").addEventListener("mouseover",mouseOverColorE);
- document.getElementById("decodeButton").addEventListener("mouseover",mouseOverColorD);
- 
- document.getElementById("encodeButton").addEventListener("mouseout",mouseOutColorE);
- document.getElementById("decodeButton").addEventListener("mouseout",mouseOutColorD);
-
-
- function mouseOverColorE() {
-    document.getElementById("encodeButton").style.backgroundColor = '#fbf7fc';
-    document.getElementById("encodeButton").style.color = '#626fa6';
-    document.getElementById("encodeButton").style.border = '#626fa6';
-    document.getElementById("encodeButton").style.border = '1px solid #626fa6'; 
- }
-
- function mouseOverColorD() {     
-    document.getElementById("decodeButton").style.backgroundColor = '#fbf7fc';
-    document.getElementById("decodeButton").style.color = '#626fa6';
-    document.getElementById("decodeButton").style.border = '#626fa6';
-    document.getElementById("decodeButton").style.border = '1px solid #626fa6'; 
- }
-
+function mouseOverColorD() {     
+    document.getElementById("decode-button").style.backgroundColor = '#fbf7fc';
+    document.getElementById("decode-button").style.color = '#626fa6';
+    document.getElementById("decode-button").style.border = '#626fa6';
+    document.getElementById("decode-button").style.border = '1px solid #626fa6'; 
+}
 
 function mouseOutColorE() {
-    document.getElementById("encodeButton").style.backgroundColor = '#626fa6';
-    document.getElementById("encodeButton").style.color = '#fbf7fc';
-    document.getElementById("encodeButton").style.border = '1px solid #fbf7fc';   
+    document.getElementById("encode-button").style.backgroundColor = '#626fa6';
+    document.getElementById("encode-button").style.color = '#fbf7fc';
+    document.getElementById("encode-button").style.border = '1px solid #fbf7fc';   
 }
 
 function mouseOutColorD() {
-    document.getElementById("decodeButton").style.backgroundColor = '#626fa6';
-    document.getElementById("decodeButton").style.color = '#fbf7fc';
-    document.getElementById("decodeButton").style.border = '1px solid #fbf7fc';
+    document.getElementById("decode-button").style.backgroundColor = '#626fa6';
+    document.getElementById("decode-button").style.color = '#fbf7fc';
+    document.getElementById("decode-button").style.border = '1px solid #fbf7fc';
 }
